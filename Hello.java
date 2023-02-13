ee4adbb76a0b564efcd0278e12e2a13db5c05a6f
E41221768_DivyaFransiskaMaharani_B <125243202+divyafransiska2903@users.noreply.github.com>
2023-02-13 15:49:34 +0700

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,16 @@
+
+package hello;
+
+/**
+ *
+ * @author DIVYA
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("DIVYA FRANSISKA MAHARANI");
+        System.out.println("E41221768");
+        System.out.println("GOLONGAN B");
+    }
+    
+}
